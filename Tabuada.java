215f08f74798f29bd7633e9d74177b57986f849a
Pedro Henrique R <104581824+20Pedroh@users.noreply.github.com>
2023-06-30 08:19:13 -0300

    Update Tabuada.java
    Agora é possível escolher outro numero ou parar

--- Tabuada.java
+++ Tabuada.java
@@ -4,17 +4,20 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Bem Vindo");
         int valor;
+
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Digite um valor");
+        System.out.println("Digite um valor ou 0 para sair");
         valor = teclado.nextInt();
+        do{
         /*Estrutura de repetição FOR:
          * i=0 representa o inicio
          * i=10 representa o fim
          * i++ represenra incremento*/
-        System.out.println("resultado da tabuada de:" + valor);
+        System.out.println("Resultado da tabuada de:" + valor);
         for (int i = 0; i <= 10;i++){
             System.out.println(valor + "x" + i + "=" + (valor*i) );
         }
-        
+        valor = teclado.nextInt();
+    }while(valor !=0);  
     }
 }
